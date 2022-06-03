fd328410a913f16c923acb0e6ddded61f6c66749
Mianala Loharano <mianalaloharano@gmail.com>
2022-06-03 20:11:15 +0300

    get songs

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const all_songs = songs();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
