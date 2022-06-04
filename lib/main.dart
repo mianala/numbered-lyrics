82a87722b03e973f33038a46d25a678a9e5607f3
Mianala <mianalaloharano@gmail.com>
2022-06-04 17:51:49 +0300

    song page appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'dart:async';
-import 'dart:io';
 import 'song.dart';
 import 'database_helper.dart';
-import 'package:flutter/services.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,9 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hira Fiderana',
-      home: const HomeScreen(),
+      home: const SongGridViewScreen(),
       routes: {
         "/grid": (context) => const SongGridViewScreen(),
+        "/list": (context) => const SongListScreen(),
         "/song": (context) => const SongPage(
               song: Song(
                 id: 1,
@@ -36,79 +32,107 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class SongListScreen extends StatelessWidget {
+  const SongListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hira Fiderana'),
-      ),
-      body: FutureBuilder(
-          future: DatabaseHelper().songs(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      title: Text(
-                          "${snapshot.data[index].number} ${snapshot.data[index].title})"),
-                      subtitle: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                              "🎹 ${snapshot.data[index].key} 📃 ${snapshot.data[index].verses}"),
-                        ],
-                      ),
-                      onTap: () {
-                        // Navigator.pushNamed(context, "/song",
-                        //     arguments: {snapshot.data[index]});
-
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SongPage(
-                              song: snapshot.data[index],
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  });
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          }),
-      // bottomAppBar with icons
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.apps),
-              onPressed: () {
-                //  navigate to named route
-                Navigator.pushNamed(context, '/grid');
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.list),
-              onPressed: () {
-                // navigate to home
-                Navigator.pushReplacementNamed(context, '/');
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.article),
-              onPressed: () {
-                Navigator.pushNamed(context, '/song');
-              },
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Hira Fiderana'),
         ),
+        body: FutureBuilder(
+            future: DatabaseHelper().songs(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return GestureDetector(
+                        child: Container(
+                            decoration: const BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  color: Colors.black12,
+                                  width: 1,
+                                ),
+                              ),
+                            ),
+                            padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: 60,
+                                  child: Text(
+                                    "${snapshot.data[index].number}",
+                                    style: const TextStyle(fontSize: 16),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                                Flexible(
+                                    child: Text(
+                                  "${snapshot.data[index].title}",
+                                  style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                )),
+                              ],
+                            )),
+                        onTap: () {
+                          // Navigator.pushNamed(context, "/song",
+                          //     arguments: {snapshot.data[index]});
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SongPage(
+                                song: snapshot.data[index],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    });
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }),
+        // bottomAppBar with icons
+        bottomNavigationBar: const BottomBar());
+  }
+}
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.apps),
+            onPressed: () {
+              //  navigate to named route
+              Navigator.pushReplacementNamed(context, '/grid');
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: () {
+              // navigate to home
+              Navigator.pushReplacementNamed(context, '/list');
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.article),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, '/song');
+            },
+          ),
+        ],
       ),
     );
   }
@@ -121,25 +145,40 @@ class SongGridViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('App Bar'),
-      ),
-      body: FutureBuilder(
-          future: DatabaseHelper().songs(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              return GridView.builder(
-                  itemCount: snapshot.data.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 5),
-                  itemBuilder: (BuildContext context, int index) {
-                    return Text("${snapshot.data[index].number}");
-                  });
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          }),
-    );
+        appBar: AppBar(
+          title: const Text('Laharan\'kira'),
+        ),
+        body: FutureBuilder(
+            future: DatabaseHelper().songs(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return GridView.builder(
+                    itemCount: snapshot.data.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 5),
+                    itemBuilder: (BuildContext context, int index) {
+                      return SquaredButton(
+                        character: snapshot.data[index].number.toString(),
+                        size: 20.0,
+                        borderWidth: 1.0,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SongPage(
+                                song: snapshot.data[index],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    });
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }),
+        bottomNavigationBar: const BottomBar());
   }
 }
 
@@ -153,10 +192,98 @@ class SongPage extends StatelessWidget {
     // display song title and content
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(song.title),
+      appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(65),
+          child: AppBar(
+            title: Column(
+              children: [
+                Text(song.title, style: const TextStyle(fontSize: 20)),
+                // style subtitle
+                Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      "Tonalité ${song.key}",
+                      style: const TextStyle(
+                          fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                    Text("Fiverenana: ${song.verses.toString()}",
+                        style: const TextStyle(fontSize: 14)),
+                  ],
+                )
+              ],
+            ),
+          )),
+      body: Padding(
+        padding: const EdgeInsets.all(0.0),
+        child: Expanded(
+            child: SingleChildScrollView(
+                child: Padding(
+                    child: Text(song.content, style: TextStyle(fontSize: 22.0)),
+                    padding: const EdgeInsets.all(18.0)))),
       ),
-      body: Text(song.content),
+      bottomNavigationBar: const BottomBar(),
+    );
+  }
+}
+
+class SquaredButton extends StatefulWidget {
+  final double size;
+  final double borderWidth;
+  final String character;
+  final void Function()? onPressed;
+
+  const SquaredButton({
+    Key? key,
+    this.size = 28.0,
+    this.borderWidth = 1.0,
+    required this.character,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  State<SquaredButton> createState() => _SquaredButtonState();
+}
+
+class _SquaredButtonState extends State<SquaredButton> {
+  @override
+  Widget build(BuildContext context) {
+    final String character = widget.character;
+    final double size = widget.size;
+    final double borderWidth = widget.borderWidth;
+    final void Function()? onPressed = widget.onPressed;
+
+    return Material(
+      shape: const BeveledRectangleBorder(),
+      child: Ink(
+        child: SizedBox(
+          width: size,
+          height: size,
+          child: Container(
+            padding: const EdgeInsets.all(4.0),
+            decoration: BoxDecoration(
+              border: Border.all(width: borderWidth, color: Colors.black12),
+            ),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                primary: Colors.black38,
+              ),
+              onPressed: onPressed,
+              child: FittedBox(
+                child: Text(
+                  character,
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: size,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
