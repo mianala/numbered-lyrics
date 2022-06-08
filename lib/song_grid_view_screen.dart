4590f0185a2b991771e4682f798f22692ad4607b
Mianala <mianalaloharano@gmail.com>
2022-06-08 14:42:41 +0300

    rf grid view builder

--- lib/song_grid_view_screen.dart
+++ lib/song_grid_view_screen.dart
@@ -19,32 +19,35 @@ class SongGridViewScreen extends StatelessWidget {
             future: DatabaseHelper().songs(),
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (snapshot.hasData) {
-                return GridView.builder(
-                    itemCount: snapshot.data.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 5),
-                    itemBuilder: (BuildContext context, int index) {
-                      return SquaredButton(
-                        character: snapshot.data[index].number.toString(),
-                        size: 20.0,
-                        borderWidth: 1.0,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => SongPage(
-                                song: snapshot.data[index],
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    });
+                return gridViewBuilder(snapshot);
               } else {
                 return const Center(child: CircularProgressIndicator());
               }
             }),
         bottomNavigationBar: bottomBar(0, context));
+  }
+
+  GridView gridViewBuilder(AsyncSnapshot<dynamic> snapshot) {
+    return GridView.builder(
+        itemCount: snapshot.data.length,
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
+        itemBuilder: (BuildContext context, int index) {
+          return SquaredButton(
+            character: snapshot.data[index].number.toString(),
+            size: 20.0,
+            borderWidth: 1.0,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SongPage(
+                    song: snapshot.data[index],
+                  ),
+                ),
+              );
+            },
+          );
+        });
   }
 }
